648eb067b88f382795bbabb510c2918a3345a5be
Kavish Kartha <38027212+kavishkartha1o1@users.noreply.github.com>
2019-10-12 15:54:09 -0700

    Add files via upload

---
+++ 02_capitols.py
@@ -0,0 +1,19 @@
+import random
+
+statesList = [Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montan, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia, Wisconsin, Wyoming]
+
+randomState = random.choice(statesList)
+capitolsList = [Montogomery, Juneau, Phoenix, Little Rock, Sacramento, Denver, Hartford, Dover, Tallahassee, Atlanta, Honolulu, Boise, Springfield, Indianapolis, Des Moines, TOpeka, Frankfort, Baton Rouge, Augusta, Annapolis, Boston, Lansing, Saint Paul, Jackson, Jefferson City, Helena, Lincoln, Carson City, Concord, Trenton, Santa Fe, Albany, Raleigh, Bimarck, Columbus, Oklahoma City, Salem, Harrisburg, Providence, Columbia, Pierre, Nashville, Austin, Salt Lake Sity, Montpelier, Richmond, Olympia, Charleston, Madison,Cheyenne]
+
+capitol = ""
+
+if randomState == Alabama:
+    capitol = capitolsList[0]
+if randomState == Alaska:
+    capitol = capitolsList[1]
+if randomState == Arizona:
+    capitol = capitolsList[2]
+if randomState == Arkansas
+    
+
+print("Ready: " + randomState)
