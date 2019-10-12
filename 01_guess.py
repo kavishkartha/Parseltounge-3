29d22ac82319523894b5d8a77adc8df1bec6f907
Kavish Kartha <38027212+kavishkartha1o1@users.noreply.github.com>
2019-10-12 15:13:02 -0700

    Add files via upload

--- 01_guess.py
+++ 01_guess.py
@@ -1,6 +1,6 @@
 import random
 
-stringList = ["ape", "supreme", "white", "air", "champion"]
+stringList = ["black", "green", "white", "dough", "zebra"]
 guessCount = 10
 randomString = random.choice(stringList)
 
@@ -8,8 +8,22 @@ def randomStrSplit(randomStr):
     return [ch for ch in randomStr]
 
 randomStringSplit = randomStrSplit(randomString)
-print("The secret word begins with the letter " + randomStringSplit[0])
-print("GUESS: ")
-userGuess = input()
-userGuessSplit = randomStrSplit(userGuess)
-userGuessLen = len(userGuessSplit)
+
+while guessCount == 10:
+    guessCount -= 1
+    print("The secret word begins with the letter " + randomStringSplit[0] + " and is 5 letters long.")
+    print("GUESS: ")
+    userGuess = input()
+    userGuessSplit = randomStrSplit(userGuess)
+    userGuessLen = len(userGuessSplit)
+
+    if userGuess == "":
+        print("You wasted a guess =P")
+    if userGuessLen > 5:
+        print("0, 1, 2, 3, 4 that’s how we count to five!")
+    if 1 <= userGuessLen < 5:
+        print("0, 1, 2, 3, 4 that's how we count to five!")
+    if userGuess == randomString:
+        print("Good Job! You are one with the Source.")
+    if userGuessSplit[0] == randomStringSplit[0]:
+        print("You have " + str(guessCount) + " gueses left")
